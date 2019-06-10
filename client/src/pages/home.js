c3991f73e37eab03f5229e443343844abf862143
msesno <msesno@gmail.com>
2019-06-10 10:26:44 -0400

    update home links

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -15,13 +15,11 @@ const Example = (props) => {
         <p className="lead text-center">react app for tracking softball <br></br>players, teams and games</p>
         <hr className="my-2 text-center" />
         <p>choose a section below to begin</p>
-        <p className="btn-grp">
         <ButtonGroup>
         <Button outline color="primary" href="/players">players</Button>
         <Button outline color="primary" href="/teams">teams</Button>
         <Button outline color="primary" href="/games">games</Button>
       </ButtonGroup>
-        </p>
       </Jumbotron></Col>
       <Col size="sm-1"></Col>
     </Row></Container></div>
