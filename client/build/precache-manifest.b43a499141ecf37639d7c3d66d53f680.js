cf6a7431f7a4d905f88aed328b60f8f963160bfa
msesno <msesno@gmail.com>
2019-06-10 10:36:42 -0400

    home links

---
+++ client/build/precache-manifest.b43a499141ecf37639d7c3d66d53f680.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33c28aefe37638cdbc5e",
+    "url": "/static/css/main.7271a16e.chunk.css"
+  },
+  {
+    "revision": "33c28aefe37638cdbc5e",
+    "url": "/static/js/main.48041020.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fd66462c1e90a5477a3c",
+    "url": "/static/js/2.5d8a6732.chunk.js"
+  },
+  {
+    "revision": "fb80011626f1e90806e60ff2165b13cd",
+    "url": "/index.html"
+  }
+];
